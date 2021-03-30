ad989f7fd79ddde5350c12685cda736691733452
Jorteck <jorteckdev@gmail.com>
2021-03-30 22:36:24 +0200

    [skip ci] Add NativeArray class.

--- src/main/ICNWItem.cs
+++ src/main/ICNWItem.cs
@@ -8,13 +8,15 @@
 // the SWIG interface file instead.
 //------------------------------------------------------------------------------
 
+using System;
+
 namespace NWN.Native.API {
 
 public interface ICNWItem {
   [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
   global::System.Runtime.InteropServices.HandleRef GetInterfaceCPtr();
-  public byte[] m_nLayeredTextureColors { set; get; }
-  public unsafe byte*[] m_pLayeredTextureColorsPerPart { set; get; }
+  public NativeArray<byte> m_nLayeredTextureColors { set; get; }
+  public unsafe NativeArray<IntPtr> m_pLayeredTextureColorsPerPart { set; get; }
   public uint m_nBaseItem  { set; get; }
   byte GetLayeredTextureColorPerPart(byte nTexture, byte nPart);
   void SetLayeredTextureColorPerPart(byte nTexture, byte nPart, byte nColor);
